b4432535d4ef54f17acb4025bc79ae89f0337936
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2020-04-22 14:02:38 -0700

    Fixed an issue where you could upload two identifiers with the same name

--- backend/src/main/java/gov/nasa/jpl/fspa/service/ValidationServiceImpl.java
+++ backend/src/main/java/gov/nasa/jpl/fspa/service/ValidationServiceImpl.java
@@ -2,9 +2,7 @@ package gov.nasa.jpl.fspa.service;
 
 import gov.nasa.jpl.fspa.model.StateVariable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ValidationServiceImpl implements ValidationService {
     public boolean hasInvalidStateVariables(List<StateVariable> stateVariableList) {
@@ -23,19 +21,31 @@ public class ValidationServiceImpl implements ValidationService {
     }
 
     /**
-     * Checks for duplicate identifiers for a given list of state variables..
+     * Checks for duplicate identifiers for a given list of state variables.
      * @param stateVariableList The new list of state variables we are checking for duplicates.
      * @param identifierMap The list of current identifiers to check against.
      * @return A list of the duplicate identifiers.
      */
     public List<String> getDuplicateIdentifiers(List<StateVariable> stateVariableList, Map<String, Integer> identifierMap) {
         List<String> duplicateIdentifiers = new ArrayList<>();
+        Set<String> uploadedIdentifierSet = new HashSet<>();
 
+
+        // Check the existing identifiers we have.
         for (StateVariable stateVariable: stateVariableList) {
-            if (identifierMap.get(stateVariable.getIdentifier()) != null
-                    && !identifierMap.get(stateVariable.getIdentifier()).equals(stateVariable.getId())) {
-                duplicateIdentifiers.add(stateVariable.getIdentifier());
+            String currentIdentifier = stateVariable.getIdentifier();
+
+            // Check the uploaded state variables first and make sure the user didn't upload the same identifier more than once.
+            if (uploadedIdentifierSet.contains(currentIdentifier)) {
+                duplicateIdentifiers.add(currentIdentifier);
             }
+
+            if (identifierMap.get(currentIdentifier) != null
+                    && !identifierMap.get(currentIdentifier).equals(stateVariable.getId())) {
+                duplicateIdentifiers.add(currentIdentifier);
+            }
+
+            uploadedIdentifierSet.add(currentIdentifier);
         }
 
         return duplicateIdentifiers;
