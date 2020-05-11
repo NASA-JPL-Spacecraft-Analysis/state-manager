7c0d1f217636141c0597f770a8964b25f662a3ba
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2020-05-11 15:43:06 -0700

    Added paginator to our state variable tables

--- front-end/src/app/material/material.module.ts
+++ front-end/src/app/material/material.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -21,6 +22,7 @@ const MODULES = [
   MatIconModule,
   MatInputModule,
   MatMenuModule,
+  MatPaginatorModule,
   MatSelectModule,
   MatTableModule,
   MatToolbarModule,
