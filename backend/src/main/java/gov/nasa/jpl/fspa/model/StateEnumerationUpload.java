a3962044e7c60b159e0c69da648c5e4a60a067ed
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2020-05-12 09:33:33 -0700

    Changed state enumeration upload class to extend state enumeration:

--- backend/src/main/java/gov/nasa/jpl/fspa/model/StateEnumerationUpload.java
+++ backend/src/main/java/gov/nasa/jpl/fspa/model/StateEnumerationUpload.java
@@ -2,15 +2,9 @@ package gov.nasa.jpl.fspa.model;
 
 import com.opencsv.bean.CsvBindByName;
 
-public class StateEnumerationUpload {
+public class StateEnumerationUpload extends StateEnumeration {
     @CsvBindByName(required = true)
     private String stateIdentifier;
-
-    @CsvBindByName(required = true)
-    private String label;
-
-    @CsvBindByName(required = true)
-    private int value;
 
     public String getStateIdentifier() {
         return stateIdentifier;
@@ -18,21 +12,5 @@ public class StateEnumerationUpload {
 
     public void setStateIdentifier(String stateIdentifier) {
         this.stateIdentifier = stateIdentifier;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
     }
 }
