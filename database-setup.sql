aa805d85adf8a1e8e7a6a23684e640d204e9fea0
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2020-05-12 08:49:39 -0700

    Updated database setup to match all our new tables

--- database-setup.sql
+++ database-setup.sql
@@ -1,31 +1,69 @@
-CREATE TABLE `state_variables` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `information_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL,
   `identifier` varchar(45) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
-  `units` varchar(45) DEFAULT NULL,
-  `source` varchar(45) DEFAULT NULL,
+  `display_name` varchar(45) DEFAULT NULL,
   `description` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `identifier_UNIQUE` (`identifier`)
+  `external_link` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `state_enumerations` (
+CREATE TABLE `relationship_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `state_variable_id` int(11) NOT NULL,
-  `enum_value` varchar(45) DEFAULT NULL,
-  `value` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `state_variable_id` FOREIGN KEY (`id`) REFERENCES `state_variables` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `relationship_id` int(11) NOT NULL,
+  `display_name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `subject_type` int(11) NOT NULL,
+  `target_type` int(11) NOT NULL,
+  `subject_type_id` int(11) NOT NULL,
+  `target_type_id` int(11) NOT NULL,
+  `updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `relationships` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `display_name` varchar(45) NOT NULL,
   `description` text,
-  `subject_state_id` int(11) NOT NULL,
-  `target_state_id` int(11) NOT NULL,
-  `type` varchar(45) NOT NULL,
+  `subject_type` int(11) NOT NULL,
+  `target_type` int(11) NOT NULL,
+  `subject_type_id` int(11) NOT NULL,
+  `target_type_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `state_enumerations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `state_variable_id` int(11) NOT NULL,
+  `label` varchar(45) DEFAULT NULL,
+  `value` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `state_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `state_id` int(11) NOT NULL,
+  `identifier` varchar(45) NOT NULL,
+  `display_name` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `units` varchar(45) DEFAULT NULL,
+  `source` varchar(45) DEFAULT NULL,
+  `subsystem` varchar(45) DEFAULT NULL,
+  `description` text,
+  `updated` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `state_variables` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) NOT NULL,
+  `display_name` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `units` varchar(45) DEFAULT NULL,
+  `source` varchar(45) DEFAULT NULL,
+  `subsystem` varchar(45) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `identifier_UNIQUE` (`identifier`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
