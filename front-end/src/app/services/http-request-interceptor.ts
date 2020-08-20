a0e73bdd546ce2644019429f1eda7f99f0c4877a
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2020-08-20 09:54:11 -0700

    Added production flag check

--- front-end/src/app/services/http-request-interceptor.ts
+++ front-end/src/app/services/http-request-interceptor.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
   public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({
-      withCredentials: true
-    });
+    // Only add withCredentials when we're running in prod mode
+    if (environment.production) {
+      request = request.clone({
+        withCredentials: true
+      });
+    }
 
     return next.handle(request);
   }
