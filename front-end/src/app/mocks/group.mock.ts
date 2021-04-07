d14d13af0e3a4bfd3f8020f84ba8574dda262895
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2021-04-07 08:11:04 -0700

    Added some tests for the group reducer

---
+++ front-end/src/app/mocks/group.mock.ts
@@ -0,0 +1,20 @@
+import { Group } from './../models';
+
+export const mockGroup1: Group = {
+  collectionId: '1-collection',
+  groupMappings: [],
+  id: '1-group',
+  name: 'Test Group'
+};
+
+export const mockGroup2: Group = {
+  collectionId: '1-collection',
+  groupMappings: [],
+  id: '2-group',
+  name: 'Test Group'
+};
+
+export const mockGroups: Group[] = [
+  mockGroup1,
+  mockGroup2
+];
