808d14ec62a805b5b485186eb5e39987c8bc44bd
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2022-03-08 12:27:16 -0800

    Added a 5 minute cache timer to data types load

--- backend/src/service/data-types.service.ts
+++ backend/src/service/data-types.service.ts
@@ -5,11 +5,17 @@ import { DataType } from '../models';
 @Service()
 export class DataTypesService {
   private dataTypeMap: Map<string, Set<string>>;
+  private lastLoadTime: Date;
+
+  // Cache the data types for 5 minutes.
+  private readonly CACHE_TIME = 300000;
 
   public async getDataType(type: string): Promise<Set<string>> {
-    // If we haven't loaded our data types yet, do that now.
-    if (!this.dataTypeMap) {
+    // If we haven't loaded our data types yet, or we should refresh them after the CACHE_TIME, do that now.
+    if (!this.dataTypeMap || new Date(Date.now() - 1000) > this.lastLoadTime) {
       await this.loadDataTypes();
+
+      this.lastLoadTime = new Date();
     }
 
     return this.dataTypeMap.get(type) ?? new Set();
