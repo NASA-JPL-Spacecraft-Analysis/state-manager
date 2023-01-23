0155b77b19e1bb69772cda6878df4048de153182
auman <cydney.m.auman@jpl.nasa.gov>
2023-01-23 10:42:30 -0800

    update to 7.0

--- backend/src/resolvers/version.resolver.ts
+++ backend/src/resolvers/version.resolver.ts
@@ -2,7 +2,7 @@ import { Query, Resolver } from 'type-graphql';
 
 import { Version } from '../models/version';
 
-const VERSION = 'V4.0 G23.0';
+const VERSION = 'V7.0 G23.0';
 
 @Resolver()
 export class VersionResolver {
