d11f76ca917e8e59c82c9cb054e5d313d6d336c9
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2023-02-07 07:32:11 -0800

    Updated version

--- backend/src/resolvers/version.resolver.ts
+++ backend/src/resolvers/version.resolver.ts
@@ -2,13 +2,12 @@ import { Query, Resolver } from 'type-graphql';
 
 import { Version } from '../models/version';
 
-const VERSION = 'V7.0 G23.0';
+const VERSION = 'V7.0.1 G23.0';
 
 @Resolver()
 export class VersionResolver {
-
-    @Query()
-    public version(): Version {
-      return { version: VERSION };
-    }
+  @Query()
+  public version(): Version {
+    return { version: VERSION };
+  }
 }
