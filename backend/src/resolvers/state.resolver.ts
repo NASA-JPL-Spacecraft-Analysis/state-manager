f441d65db82570b6eabe8dea5ce469357716f1c7
winner <winner@jpl.nasa.gov>
2023-05-15 13:40:13 -0700

    make async

--- backend/src/resolvers/state.resolver.ts
+++ backend/src/resolvers/state.resolver.ts
@@ -264,34 +264,31 @@ export class StateResolver implements ResolverInterface<State> {
   }
 
   @Query(() => [String])
-  public statesCsv(@Args() { collectionId }: CollectionIdArgs): Promise<string[]> {
-    var states = State.find({
+  public async statesCsv(@Args() { collectionId }: CollectionIdArgs): Promise<string[]> {
+    var states = await State.find({
       where: {
         collectionId
       }
     });
     var states_str = "channelId,dataType,description,displayName,editable,externalLink,id,identifier,restricted,source,subsystem,type,units,version\n"
-    var states_return = states.then((result => {
-      for (var i in result) {
-        states_str += `${result[i].channelId},`+
-                        `${result[i].dataType},`+
-                        `${result[i].description},`+
-                        `${result[i].displayName},`+
-                        `${result[i].editable},`+
-                        `${result[i].externalLink},`+
-                        `${result[i].id},`+
-                        `${result[i].identifier},`+
-                        `${result[i].restricted},`+
-                        `${result[i].source},`+
-                        `${result[i].subsystem},`+
-                        `${result[i].type},`+
-                        `${result[i].units},`+
-                        `${result[i].version}\n`
+    for (var i in states) {
+      states_str += `${states[i].channelId},`+
+                    `${states[i].dataType},`+
+                    `${states[i].description},`+
+                    `${states[i].displayName},`+
+                    `${states[i].editable},`+
+                    `${states[i].externalLink},`+
+                    `${states[i].id},`+
+                    `${states[i].identifier},`+
+                    `${states[i].restricted},`+
+                    `${states[i].source},`+
+                    `${states[i].subsystem},`+
+                    `${states[i].type},`+
+                    `${states[i].units},`+
+                    `${states[i].version}\n`
       }
-      var states_promise = new Promise<string[]>((resolve) => resolve([states_str]))
-      return states_promise
-    }));
-    return states_return
+    var states_promise = new Promise<string[]>((resolve) => resolve([states_str]))
+    return states_promise
   }
 
   @Query(() => [String])
