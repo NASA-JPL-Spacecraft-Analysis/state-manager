2668968b448e9c23c3003a8cfd2571efc089df2a
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2023-09-13 14:12:51 -1000

    Added dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { createConnection } from 'typeorm';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import { Container } from 'typedi';
+import 'dotenv/config';
 
 import {
   CollectionResolver,
@@ -21,9 +22,36 @@ import {
 } from './resolvers/';
 
 const main = async () => {
-  await createConnection();
+  const host = process.env.DB_URL;
+  const password = process.env.DB_PASS;
+  const port = process.env.DB_PORT;
+  const database = process.env.DB_NAME;
+  const username = process.env.DB_USER;
 
-  const schema = await buildSchema({
+  if (
+    password !== undefined &&
+    port !== undefined &&
+    database !== undefined &&
+    host !== undefined &&
+    username !== undefined
+  ) {
+    await createConnection({
+      password,
+      port: Number(port),
+      database,
+      host,
+      username,
+      type: 'mysql',
+      entities: ['src/models/*.ts'],
+      synchronize: false
+    });
+  } else {
+    throw new Error(
+      'Could not connect to database, please make sure DB_PASS, DB_PORT, DB_NAME, DB_URL, and DB_USER are defined in your environment'
+    );
+  }
+
+  const dbSchema = await buildSchema({
     container: Container,
     resolvers: [
       CollectionResolver,
@@ -57,7 +85,7 @@ const main = async () => {
         'request.credentials': 'include'
       }
     },
-    schema,
+    schema: dbSchema,
     tracing: true
   });
 
